5bca253f1dbcd8dda3fea20f6aaecf0dc0a1c57b
つくも <92912896+Thukumo@users.noreply.github.com>
2025-05-12 14:36:33 +0900

    遅延評価を入れた なんと普通の処理より遅い

--- src/define_type.rs
+++ src/define_type.rs
@@ -1,12 +1,11 @@
 use std::{collections::HashMap, ops::Index};
 
-#[derive(Debug)]
-#[allow(dead_code)]
-pub struct TypeUnmatchedError(String);
+use crate::parse_value;
 
 #[derive(Debug)]
 pub enum ParseError {
     InvalidData(String),
+    TypeUnmatchedError(String),
     ParseFloatError(std::num::ParseFloatError),
     ParseIntError(std::num::ParseIntError),
 }
@@ -32,12 +31,12 @@ pub enum Number {
 macro_rules! impl_try_into {
     ($enum_type:ty, $variant:ident, $output:ty) => {
         impl TryInto<$output> for $enum_type {
-            type Error = TypeUnmatchedError;
+            type Error = crate::ParseError;
             fn try_into(self) -> Result<$output, Self::Error> {
                 if let Self::$variant(value) = self {
                     Ok(value)
                 } else {
-                    Err(TypeUnmatchedError(format!("Expected {}::{}", stringify!($enum_type), stringify!($variant))))
+                    Err(ParseError::TypeUnmatchedError(format!("Expected {}::{}", stringify!($enum_type), stringify!($variant))))
                 }
             }
         }
@@ -80,6 +79,53 @@ impl Index<&str> for JsonValue {
 }
 
 impl Index<String> for JsonValue {
+    type Output = Self;
+    fn index(&self, index: String) -> &Self::Output {
+        self.index(index.as_str())
+    }
+}
+
+#[derive(Debug)]
+pub enum JsonValueLazy {
+    Something(String),
+    Object(HashMap<String, JsonValueLazy>),
+    Array(Vec<JsonValueLazy>),
+    InValidLocation,
+}
+
+impl Into<JsonValue> for JsonValueLazy {
+    fn into(self) -> JsonValue {
+        match self {
+            JsonValueLazy::Something(val) => {
+                parse_value(&val).unwrap()
+            }
+            JsonValueLazy::Array(val) => {
+                JsonValue::Array(val.into_iter().map(|j| j.into()).collect())
+            }
+            JsonValueLazy::Object(val) => {
+                JsonValue::Object(val.into_iter().map(|(key, val)| {
+                    (key, val.into())
+                }).collect::<HashMap<String, JsonValue>>())
+            }
+            JsonValueLazy::InValidLocation => {
+                JsonValue::InValidLocation
+            }
+        }
+    }
+}
+
+impl Index<&str> for JsonValueLazy {
+    type Output = Self;
+    fn index(&self, index: &str) -> &Self::Output {
+        if let Self::Object(map) = self {
+            map.get(index).unwrap_or_else(|| &Self::InValidLocation)
+        } else {
+            &Self::InValidLocation
+        }
+    }
+}
+
+impl Index<String> for JsonValueLazy {
     type Output = Self;
     fn index(&self, index: String) -> &Self::Output {
         self.index(index.as_str())
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
-use crate::{JsonValue, Number, ParseError};
+use crate::{JsonValue, JsonValueLazy, Number, ParseError};
 use std::collections::HashMap;
 
-fn parse_value(s: &str) -> Result<JsonValue, ParseError> {
+pub(crate) fn parse_value(s: &str) -> Result<JsonValue, ParseError> {
     Ok(
         if s.starts_with('"') {
             JsonValue::String(s[1..s.len() - 1].to_string())
@@ -19,7 +19,7 @@ fn parse_value(s: &str) -> Result<JsonValue, ParseError> {
     )
 }
 
-fn parse_obj(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseError> {
+pub(crate) fn parse_obj(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseError> {
     let mut result = HashMap::new();
     let mut key = None;
     let (first_pos, mut pos) = (pos, pos+1);
@@ -53,7 +53,7 @@ fn parse_obj(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseE
     }
 }
 
-fn parse_arr(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseError> {
+pub(crate) fn parse_arr(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseError> {
     let (first_pos, mut pos) = (pos, pos+1);
     let mut result = Vec::with_capacity(tokens.len()/2);
 
@@ -84,6 +84,115 @@ fn parse_arr(tokens: &[String], pos: usize) -> Result<(usize, JsonValue), ParseE
 
 pub fn parse(s: &str) -> Result<JsonValue, ParseError> {
     Ok(parse_obj(&s.split('"')
+        .fold((Vec::new(), String::new(), true), |state, s| {
+        let (mut state, mut current, odd) = state;
+        if s.ends_with('\\') {
+            current.reserve_exact(s.len() + 1);
+            current.push_str(s);
+            current.push('"');
+            (state, current, odd)
+        } else {
+            current.reserve_exact(s.len());
+            current.push_str(s);
+            if odd {
+                state.extend(current.chars().fold((Vec::new(), false), |state, c| {
+                    let (mut state, splitter) = state;
+                    if c.is_whitespace() {
+                        (state, splitter)
+                    } else {
+                        match c {
+                            '{' | '}' | '[' | ']' | ':' | ',' => {
+                                state.push(c.to_string());
+                                (state, true)
+                            }
+                            _ => {
+                                if splitter {
+                                    state.push(c.to_string());
+                                } else {
+                                    state.last_mut().unwrap().push(c);
+                                }
+                                (state, false)
+                            }
+                        }
+                    }
+                }).0);
+            } else {
+                let mut s_p = String::with_capacity(current.len() + 2);
+                s_p.push('"');
+                s_p.push_str(&current);
+                s_p.push('"');
+                state.push(s_p);
+            }
+            (state, String::new(), !odd)
+        }
+    }).0, 0)?.1)
+}
+
+pub(crate) fn parse_obj_lazy(tokens: &[String], pos: usize) -> Result<(usize, JsonValueLazy), ParseError> {
+    let mut result = HashMap::new();
+    let mut key = None;
+    let (first_pos, mut pos) = (pos, pos+1);
+
+    loop {
+        let token = &tokens[pos];
+        match token.as_str() {
+            "{" => {
+                let (diff, val) = parse_obj_lazy(tokens, pos)?;
+                result.insert(key.take().unwrap(), val);
+                pos += diff;
+            }
+            "}" => {
+                return Ok((pos - first_pos, JsonValueLazy::Object(result)));
+            }
+            "[" => {
+                let (diff, val) = parse_arr_lazy(tokens, pos)?;
+                result.insert(key.take().unwrap(), val);
+                pos += diff;
+            }
+            ":" | "," => {}
+            _ => {
+                if key.is_none() {
+                    key = Some(token[1..token.len() - 1].to_string());
+                } else {
+                    result.insert(key.take().unwrap(), JsonValueLazy::Something(token.clone()));
+                }
+            }
+        };
+        pos += 1;
+    }
+}
+
+pub(crate) fn parse_arr_lazy(tokens: &[String], pos: usize) -> Result<(usize, JsonValueLazy), ParseError> {
+    let (first_pos, mut pos) = (pos, pos+1);
+    let mut result = Vec::with_capacity(tokens.len()/2);
+
+    loop {
+        let token = &tokens[pos];
+        match token.as_str() {
+            "{" => {
+                let (diff, val) = parse_obj_lazy(tokens, pos)?;
+                result.push(val);
+                pos += diff;
+            }
+            "," => {}
+            "[" => {
+                let (diff, val) = parse_arr_lazy(tokens, pos)?;
+                result.push(val);
+                pos += diff;
+            }
+            "]" => {
+                return Ok((pos - first_pos, JsonValueLazy::Array(result)));
+            }
+            _ => {
+                result.push(JsonValueLazy::Something(token.clone()));
+            }
+        }
+        pos += 1;
+    }
+}
+
+pub fn parse_lazy(s: &str) -> Result<JsonValueLazy, ParseError> {
+    Ok(parse_obj_lazy(&s.split('"')
         .fold((Vec::new(), String::new(), true), |state, s| {
         let (mut state, mut current, odd) = state;
         if s.ends_with('\\') {
